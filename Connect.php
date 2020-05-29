2a72fa93e2853f9d56f878f390448d052681b243
secretjunio <44667497+secretjunio@users.noreply.github.com>
2020-05-29 19:30:20 +0700

    Add files via upload
    file connect này dùng chung cho tất cả các trang nào cần truy vấn dữ liệu nha

---
+++ Connect.php
@@ -0,0 +1,24 @@
+<?php
+    $connect=mysqli_connect("localhost","root","","database_webbanlaptop");
+    /*
+    $sql="SELECT * FROM admin";
+    // Ki?m tra k?t n?i
+    if ($connect->connect_error) {
+     die("K?t n?i th?t b?i: " . $conn->connect_error);
+    } 
+ 
+    $result = mysqli_query($connect, $sql);
+    if ($result) {
+    // Hàm `mysql_fetch_row()` s? ch? fetch d? li?u m?t record m?i l?n ???c g?i
+    // do ?ó c?n s? d?ng vòng l?p While ?? l?p qua toàn b? d? li?u trên b?ng posts
+    while ($row=mysqli_fetch_row($result)) {
+        printf ("Id: %s, Taikhoan: %s, Matkhau: %s ,Chuc vu:%s,Trang thai:%s<br>",$row[0],$row[1], $row[2],$row[3],$row[4]);
+    }
+
+    // Máy tính s? l?u k?t qu? t? vi?c truy v?n d? li?u b?ng
+    // Do ?ó chúng ta nên gi?i phóng b? nh? sau khi hoàn t?t ??c d? li?u
+    mysqli_free_result($result);
+    }
+    */
+   
+?>
